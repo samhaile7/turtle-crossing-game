73ccbd0904d6583d8cc2b04e86680af27f35ab6e
Samuel Haile <samhaile129@gmail.com>
2022-11-15 21:41:59 -0500

    Added player and car classes

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,27 @@
+from turtle import Turtle
+import random
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
+START_POS = (random.randrange(300,350,10), random.randrange(-200,200, 10))
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.hideturtle()
+        self.goto(START_POS)
+        self.shape("square")
+        self.shapesize(2, 1)
+        self.color(random.choice(COLORS))
+        self.showturtle()
+        self.setheading(180)
+
+
+        self.move_left()
+
+    def move_left(self):
+        self.move_left(MOVE_INCREMENT)
+
+
--- main.py
+++ main.py
@@ -8,8 +8,14 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player = Player()
+
+screen.listen()
+screen.onkeypress(player.move_up, "Up")
+screen.onkeypress(player.move_down, "Down")
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
-print('test')
+screen.exitonclick()
